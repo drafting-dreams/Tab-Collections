ea8ac58006be74024dce186a675f2f81f8b40327
liuxiaoyang <xiaoyang.liu@shopee.com>
2021-08-29 21:49:20 +0800

    refactor: adapt manifestV3 service worker

--- background/db/connect.js
+++ background/db/connect.js
@@ -3,19 +3,19 @@ let db
 
 export function connectDB(cb) {
   // In the following line, you should include the prefixes of implementations you want to test.
-  if (!window.indexedDB) {
-    window.indexedDB = window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB
+  if (!self.indexedDB) {
+    self.indexedDB = self.mozIndexedDB || self.webkitIndexedDB || self.msIndexedDB
   }
   // DON'T use "var indexedDB = ..." if you're not in a function.
   // Moreover, you may need references to some window.IDB* objects:
   // window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || { READ_WRITE: 'readwrite' } // This line should only be needed if it is needed to support the object's constants for older browsers
   // window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
   // (Mozilla has never prefixed these objects, so we don't need window.mozIDB*)
-  if (!window.indexedDB) {
+  if (!self.indexedDB) {
     throw new Error("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.")
   }
 
-  const openRequest = window.indexedDB.open('TabsCollectionIDB', 1)
+  const openRequest = self.indexedDB.open('TabsCollectionIDB', 2)
 
   openRequest.onsuccess = function (event) {
     db = openRequest.result
